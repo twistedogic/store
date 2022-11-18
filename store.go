d2a91edb3f1bb4fae82be7c6633852f23387a676
Yat Yee Li <twistedogic@gmail.com>
2022-11-19 03:52:10 +0800

    update

--- store.go
+++ store.go
@@ -2,15 +2,20 @@ package store
 
 import (
 	"context"
+	"errors"
 	"io"
 )
 
+var KeyNotFound = errors.New("key not found")
+
 type Getter interface {
-	Get(context.Context, string, io.Writer) (bool, error)
+	// Get writes value of key into w if key exists
+	// exist is true if key exists
+	Get(ctx context.Context, key string, w io.Writer) (err error)
 }
 
 type Setter interface {
-	Set(context.Context, io.Reader, ...Metadata) error
+	Set(context.Context, string, io.Reader) error
 }
 
 type Deleter interface {
